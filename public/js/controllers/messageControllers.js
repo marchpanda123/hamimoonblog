f63de6758e44fea3bce0f9689580d6f731dc4c10
jiawei <jiawei.paris@gmail.com>
2017-06-14 19:58:39 +0200

    2017/6/14 third update

--- public/js/controllers/messageControllers.js
+++ public/js/controllers/messageControllers.js
@@ -1,17 +1,14 @@
 (function() {
 	angular.module('startApp').controller('messageCtrl',[
 		'$scope',
-		'$http',
 		'MessageFactory',
 		'AuthFactory',
 		function($scope,
-			$http,
 			MessageFactory,
 			AuthFactory
 			) {
 			$scope.isAuth = AuthFactory.isAuthenticated();
 			$scope.isAdmin = AuthFactory.isAdmin();
-
 			$scope.currentMessages = null;
 
 			$scope.listMessages = function() {
@@ -19,15 +16,13 @@
 			}
 
 			$scope.createMessage = function(message) {
+				console.log(message);
 				new MessageFactory(message).$create()
 				.then(function(newMessage) {
 					$scope.messages.push(newMessage);
 					window.location.reload();
 				});
 			}
-
-			
-
 			$scope.listMessages();
 		}]);	
 })();
